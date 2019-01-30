4d460c320bf5c5b72f23974a9d71dc8de23e65fb
OpSecIsSexy <michael.sturd@gmail.com>
2019-01-30 15:21:59 -0600

    Initial commit
    Set up basic structure of the suite.

---
+++ canvas-enhancement-suite/duedates.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#
+# Module for using the Canvas API to:
+# - Pull asignment data from a module
+# - Format data into human readable format
+# - Save formatted data in a document
+
---
+++ canvas-enhancement-suite/start.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#
+# Python module to:
+# - Start canvas-enhancement-suite
+# - Interact with user
+#
